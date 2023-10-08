9db343b08b9e3b8f553318e819ef32703e5969e6
Totheph <low.mikow@gmail.com>
2023-10-08 17:56:06 +0300

    Changed names + tablename

--- webapp/db.py
+++ webapp/db.py
@@ -7,5 +7,5 @@ import psycopg2
 engine = create_engine("postgresql://vqklygsa:5C42T__du4u1BsNdcbgU9e5P8jNmpGyk@cornelius.db.elephantsql.com/vqklygsa")
 db_session = scoped_session(sessionmaker(bind=engine))
 
-db = declarative_base()
-db.query = db_session.query_property()
+Base = declarative_base()
+Base.query = db_session.query_property()
