e69abe97244acf72edaecf0662cd6d50fa85e69c
Oleinikov Vasily <vasia94.94@mail.ru>
2023-10-21 13:06:41 +0300

    убрал ненужный импорт

--- webapp/patient/views.py
+++ webapp/patient/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for,redirect
+from flask import render_template
 from flask import Blueprint
 from datetime import datetime
 from webapp.patient.forms import NewPatient, Time
