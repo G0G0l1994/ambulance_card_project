0e43bab384ce116492220b406380d51a145d14b9
Oleinikov Vasily <vasia94.94@mail.ru>
2023-10-21 14:02:45 +0300

    после rebase

--- webapp/patient/views.py
+++ webapp/patient/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, flash
 from flask import Blueprint
 from datetime import datetime
 from webapp.patient.forms import NewPatient, Time
+from webapp.card.forms import CardFormGeneral
+from webapp.card.models import Complaint, Anamnesis
 from flask import request
+from webapp.db import db_session
 
 blueprint = Blueprint('patient', __name__, url_prefix='/new_patient')
 
@@ -31,6 +34,22 @@ def create():
   else:
     return render_template("patient.html",page_title=title, form=patient_form, time_form = time_form, time = time_dict )
 
+@blueprint.route('/main_card', methods=["GET", 'POST'])
+def main_card():
+  form = CardFormGeneral()
+  return render_template('main_card.html', form=form)
+
+@blueprint.route('/main_card', methods=["GET", 'POST'])
+def main_card_forms():
+  form = CardFormGeneral()
+  if form.validate_on_submit():
+    anamnesis = Anamnesis(anamnesis=form.anamnesis.data)
+    db_session.add(anamnesis)
+    db_session.commit()
+    complaint = Complaint(complaint=form.complaints.data)
+    db_session.add(complaint)
+    db_session.commit()
+    flash("Отправлено")
 
          
     
