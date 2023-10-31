7eebcb9e6caaf3160fe4c0abe85f942f63df32b1
Oleinikov Vasily <vasia94.94@mail.ru>
2023-10-31 17:48:04 +0300

    доработал модель карты

--- webapp/card/models.py
+++ webapp/card/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, Time, Float, Boolean, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, Time, Float, Boolean, Text
 from webapp.db import Base
 
 
@@ -8,7 +8,7 @@ class Card(Base):
     id = Column(Integer, primary_key = True)
     patient_id = Column(Integer,  index=True)
     doctor_id = Column(Integer, index=True)
-    
+    #время
     date_card = Column(Date) # дата карты
     time_of_receipt = Column(Time) # время приёма
     transmission_time = Column(Time) # время передачи
@@ -17,66 +17,53 @@ class Card(Base):
     start_time_of_hospitalization = Column(Time) #время начала госпитализации
     time_of_arrival_at_hospital = Column(Time) #время прибытия в больницу
     call_end_time = Column(Time) # время окончания вызова
-    
+    #общие сведения
     сomplaint = Column(Text)
     anamnesis = Column(Text)
-    general_assessment = Column(String())
-    сonsciousness = Column(String()) #сознание
+    general_assessment = Column(String)
+    сonsciousness = Column(String) #сознание
     glasgow_scale = Column(Integer) #шкала Глазго
-    body_position = Column(String()) # положение тела
-    temperature = Column(Float)
-    respiratory_rate = Column(Integer)
-    heartbite = Column(Integer)
-    saturation = Column(Integer)
-    pulse = Column(Integer)
-    blood_pressure_systolic = Column(Integer)
-    blood_pressure_diastolic = Column(Integer)
+    body_position = Column(String) # положение тела
+    #показатели до 
+    temperature_before = Column(Float)
+    respiratory_rate_before = Column(Integer)
+    heartbite_before = Column(Integer)
+    saturation_before = Column(Integer)
+    pulse_before = Column(Integer)
+    blood_pressure_systolic_before = Column(Integer)
+    blood_pressure_diastolic_before = Column(Integer)
     normal_blood_pressure_systolic = Column(Integer) 
     normal_blood_pressure_diastolic = Column(Integer)
-    blood_glucose = Column(Float)
-    dry_skin = Column (String())
+    blood_glucose_before = Column(Float)
+    #кожные покровы
+    dry_skin = Column (String)
     color_skin = Column(String)
-    jaundice = Column(String()) #желтушность
-    rash = Column(String()) #сыпь
-    throat = Column(String()) #зев
-    tonsils = Column(String())#миндалины
-    lymph_nodes = Column(String()) #лимфоузлы
-    swelling = Column(String()) #отёки
-    respiratory_type = Column(String())
-    wheezing = Column(String())# хрипы
-    wheezing_localisation = Column(String())# хрипы
-    dyspnea = Column(String()) #одышка
+    jaundice = Column(String) #желтушность
+    rash = Column(String) #сыпь
+    throat = Column(String) #зев
+    tonsils = Column(String)#миндалины
+    lymph_nodes = Column(String) #лимфоузлы
+    swelling = Column(String) #отёки
+    #дыхательная система
+    respiratory_type = Column(String)
+    wheezing = Column(String)# хрипы
+    wheezing_localisation = Column(String)# хрипы
+    dyspnea = Column(String) #одышка
     heart_rate_deficit = Column(Boolean)
-    heart_tone_accent = Column(Boolean) #акцент тона
-    rhythmic_tone = Column(Boolean)
-    arrhythmic_tone = Column(Boolean)
-    clear = Column(Boolean)
-    muffled = Column(Boolean)# глухие тоны
-    muted = Column(Boolean)# приглушенные
-    none_sounds = Column(Boolean) #отсутствуют
-    none_murmur = Column(Boolean)
-    systolic = Column(Boolean)
-    diastolic = Column(Boolean)
-    pericardial_friction_rub = Column(Boolean)
-    rhythmic_pulse = Column(Boolean)
-    arrhythmic_pulse = Column(Boolean)
-    weak = Column(Boolean)
-    thready = Column(Boolean)# нитевидный
-    tense = Column(Boolean)# напряжённый
-    non_pulse = Column(Boolean)
-    liver = Column(String()) # печень
-    frequency_of_bowel_movement = Column(String())# частота стула
-    abdomens_soft = Column(Boolean)
-    painless_stomach = Column(Boolean)
-    pain_stomach = Column(Boolean)
-    tense = Column(Boolean) #напряжённый
-    swollen = Column(Boolean) # вздут
+    heart_tone_accent = Column(String) #акцент тона
+    rhythmic_tone = Column(String)
+    tone_of_heart=Column(String)
+    murmur = Column(String)
+    rhythmic_pulse = Column(String)
+    characteristic_pulse = Column(String)
+    #живот
+    liver = Column(String) # печень   
+    pain_stomach = Column(String)
+    characteristic_stomach = Column(String)
     involved_in_the_act_of_breathing = Column(Boolean)
-    formed_stool = Column(Boolean) #оформленый стул
-    thin_stool = Column(Boolean) #разжижен
-    unformed_stool = Column(Boolean) #жидкий
-    regular_stool = Column(Boolean)
-    irregular_stool = Column(Boolean)
+    formed_type_stool = Column(String)
+    regular_stool = Column(String)
+    rate_stool = Column(Integer)
     is_shchetkin_blumberg = Column(Boolean)
     is_voskresensky = Column(Boolean)
     is_ortner = Column(Boolean)
@@ -85,40 +72,32 @@ class Card(Base):
     is_sitkovsky = Column(Boolean)
     is_obraztsova = Column(Boolean)
     is_murphy = Column(Boolean)
-    behavior = Column(String())
-    reaction_to_light = Column(String())
-    pupils_of_the_eyes = Column(String())
+    #нервная система
+    behavior = Column(String)
+    reaction_to_light = Column(String)
+    pupils_of_the_eyes = Column(String)
     anisocoria = Column(Boolean)
     nystagmus = Column(Boolean)
     focal_sings = Column(Boolean)
-    speech = Column(String())
+    speech = Column(String)
     none_symptoms = Column(Boolean)
     nuchal_rigidity = Column(Boolean)
     is_kernig_symptom = Column(Boolean)
     is_brudzinski_symptom = Column(Boolean)
-    none_paralysis = Column(Boolean)
-    left = Column(Boolean)
-    right = Column(Boolean)
-    saved = Column(Boolean)
-    missing_sens = Column(Boolean)
-    reduced = Column(Boolean)
-    left_sens = Column(Boolean)
-    right_sens = Column(Boolean)
+    paralysis = Column(String)
+    sensitive = Column(String)
+    #мочеполовая система
     kidney_punch = Column(String) #симптом покалачивания
-    urine = Column(String)
-    painless = Column(Boolean)
-    free = Column(Boolean)
-    painful_urine = Column(Boolean)
-    difficult = Column(Boolean)
-    missing_urine = Column(Boolean)
-    light_yellow = Column(Boolean)
-    cloudy = Column(Boolean)
-    with_inclusions = Column(Boolean)
-    with_sediment = Column(Boolean)
+    characteristic_urine = Column(String)
+    characteristic_urination = Column(String)
+    #статус локалис
     status_localis = Column(Text)
+    #ЭКГ
     ecg_before = Column(Text)
     ecg_after = Column(Text)
+    #помощь
     aid = Column(Text)
+    #показатели после
     temperature_after = Column(Float)
     respiratory_rate_after = Column(Integer)
     heartbite_after = Column(Integer)
@@ -127,4 +106,5 @@ class Card(Base):
     blood_pressure_systolic_after = Column(Integer)
     blood_pressure_diastolic_after = Column(Integer)
     blood_glucose_after = Column(Float)
+    #диагноз
     diagnosis = Column(String)
