1932da56699039c6ed50e4782307e3ab8304f498
Oleinikov Vasily <vasia94.94@mail.ru>
2023-12-09 11:29:22 +0300

    использование переменных окружения, основа readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ambulance_card_project
+Приложение служит для электронного документооборота службы Скорой Медицинской Помощи
+
+для запуска необходимо:
+1. установить библиотеки из requirements.txt
+2. создать файл .env, где указать:
+    SQLALCHEMY_DATABASE_URI = "URL вашей базы данных"
+    SECRET_KEY = "ваш секретный ключ"
+    user="пользователь базы данных"
+    password='пароль базы данных'
+    host='хост для соединения к базе данных'
+    port='порт базы данных' 
+3. в командной строке корня проекта:
+     Windows: set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask run
+     Linux/Mac: export FLASK_APP=webapp && export FLASK_ENV=development && flask run
--- webapp/config.py
+++ webapp/config.py
@@ -1,8 +1,13 @@
+import os
+from dotenv import load_dotenv
+
 from datetime import timedelta
 import psycopg2
-SECRET_KEY = "ewfrejugjrtgbjrtbtrhnbnrtbnttbtjgijitgejv nrjergegtgnrtrtjrirtjgrngrogorjgorhgrhr"
 
-REMEMBER_COOKIE_DURATION = timedelta(days=30)
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+REMEMBER_COOKIE_DURATION = timedelta(days=2)
 SQLALCHEMY_TRACK_MODIFICATION = False
-SQLALCHEMY_DATABASE_URI = "postgresql://vqklygsa:5C42T__du4u1BsNdcbgU9e5P8jNmpGyk@cornelius.db.elephantsql.com/vqklygsa"
-conn = psycopg2.connect(user="vqklygsa", password='5C42T__du4u1BsNdcbgU9e5P8jNmpGyk', host='cornelius.db.elephantsql.com', port='5432')
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+conn = psycopg2.connect(user=os.getenv('user'), password=os.getenv('password'), host=os.getenv('host'), port=os.getenv('port'))
